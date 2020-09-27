025fcc8d153fd3bd88ca678b460c089f2ec5525b
Yahya Almubarak <yahya.sj.almubarak@gmail.com>
2020-09-27 07:06:22 +0300

    First Commit

---
+++ Summation/src/Sum3.java
@@ -0,0 +1,15 @@
+
+public class Sum3 {
+
+	public static void main(String[] args) {
+		int a = 50;
+		int b = 7;
+		int c = 12;
+		int d = a + b + c;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
